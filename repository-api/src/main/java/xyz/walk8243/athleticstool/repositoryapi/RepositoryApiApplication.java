49b114811d5443417842e43689896207cfb91d19
walk8243 <walk8243@gmail.com>
2023-04-21 00:57:37 +0900

    repository-apiのinitial

---
+++ repository-api/src/main/java/xyz/walk8243/athleticstool/repositoryapi/RepositoryApiApplication.java
@@ -0,0 +1,13 @@
+package xyz.walk8243.athleticstool.repositoryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryApiApplication.class, args);
+	}
+
+}
---
+++ repository-api/src/test/java/xyz/walk8243/athleticstool/repositoryapi/RepositoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.walk8243.athleticstool.repositoryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
